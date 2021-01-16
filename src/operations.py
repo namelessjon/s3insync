7ee220bac821ded314ef8100785cec5a824bc9b4
Jon <jonathan.stott@gmail.com>
2021-01-16 16:34:22 +0000

    Add basic operation execution

--- src/operations.py
+++ src/operations.py
@@ -7,9 +7,17 @@ class Copy:
     from_repo: object
     to_repo: object
 
+    def execute(self):
+        contents = self.from_repo.contents(self.path)
+
+        self.to_repo.write(self.path, contents)
+
 
 @dc.dataclass()
 class Delete:
     path: str
     from_repo: object
     to_repo: object
+
+    def execute(self):
+        self.to_repo.delete(self.path)
