d233007e716f07087a1c0f6c3d35782c74e1e7ad
Jon <jonathan.stott@gmail.com>
2021-01-16 19:18:31 +0000

    Minimal viable aws repo

--- src/repositories.py
+++ src/repositories.py
@@ -1,10 +1,67 @@
 import dataclasses as dc
 import typing as t
+import urllib.parse as up
+
+import boto3
+
 
 @dc.dataclass(frozen=True)
 class Entry:
     path: str
     content_id: str
+
+
+@dc.dataclass()
+class Contents:
+    path: str
+    content_id: str
+    body: object
+
+    def __getattr__(self, name: str):
+        if hasattr(self.body, name):
+            return getattr(self.body, name)
+
+
+class AwsRepo:
+    def __init__(self, name: str, uri: str, client=None, maxkeys=1000):
+        self.name = name
+        self.uri = uri
+        parsed = up.urlparse(self.uri)
+        self.bucket = parsed.netloc
+        self.prefix = parsed.path[1:]
+        if client is None:
+            self.client = boto3.client('s3')
+        else:
+            self.client = client
+        self.maxkeys = maxkeys
+
+    def __iter__(self):
+        token = None
+        while True:
+            args = {
+                "Bucket": self.bucket,
+                "Prefix": self.prefix,
+                "MaxKeys": self.maxkeys,
+            }
+            if token:
+                args['ContinuationToken'] = token
+
+            response = self.client.list_objects_v2(**args)
+
+            for entry in response['Contents']:
+                key = entry['Key'][len(self.prefix) + 1:]
+                yield Entry(key, entry['ETag'].strip('"'))
+            token = response.get('NextContinuationToken')
+            if token is None:
+                break
+
+    def contents(self, path: str):
+        try:
+            obj = self.client.get_object(Bucket=self.bucket, Key=f'{self.prefix}/{path}')
+
+            return Contents(path, obj['ETag'].strip('"'), obj['Body'])
+        except self.client.exceptions.NoSuchKey:
+            raise KeyError(f"Object '{path}' not found in {self.name}")
 
 
 class TestRepo:
---
+++ tests/test_aws_repo.py
@@ -0,0 +1,74 @@
+import os
+
+import boto3
+import moto
+import pytest
+
+import src.repositories as r
+
+
+@pytest.fixture(scope='function')
+def aws_credentials():
+    """Mocked AWS Credentials for moto."""
+    os.environ['AWS_ACCESS_KEY_ID'] = 'testing'
+    os.environ['AWS_SECRET_ACCESS_KEY'] = 'testing'
+    os.environ['AWS_SECURITY_TOKEN'] = 'testing'
+    os.environ['AWS_SESSION_TOKEN'] = 'testing'
+
+
+@pytest.fixture(scope='function')
+def s3(aws_credentials):
+    with moto.mock_s3():
+        yield boto3.client('s3', region_name='us-east-1')
+
+
+@pytest.fixture()
+def aws_ab_repo(s3):
+    s3.create_bucket(Bucket="example")
+    s3.put_object(Bucket="example", Key="path/a", Body=b'a')
+    s3.put_object(Bucket="example", Key="path/b", Body=b'b')
+
+    aws = r.AwsRepo("aws", "s3://example/path", s3)
+
+    return aws
+
+
+def test_aws_repo_takes_name_s3uri():
+    aws = r.AwsRepo("aws", "s3://example/path")
+
+    assert aws.name == "aws"
+    assert aws.uri == "s3://example/path"
+
+
+def test_aws_repo_takes_name_s3uri_client(s3):
+    aws = r.AwsRepo("aws", "s3://example/path", s3)
+
+    assert aws.name == "aws"
+    assert aws.uri == "s3://example/path"
+    assert aws.client == s3
+
+
+def test_aws_repo_entries_is_files_from_the_path(aws_ab_repo):
+    entries = list(aws_ab_repo)
+
+    assert entries == [r.Entry("a", "0cc175b9c0f1b6a831c399e269772661"), r.Entry("b", "92eb5ffee6ae2fec3ad71c777531578f")]
+
+
+def test_aws_repo_entries_is_files_from_the_path_even_if_there_are_lots(aws_ab_repo):
+    aws_ab_repo.maxkeys = 1
+
+    entries = list(aws_ab_repo)
+
+    assert entries == [r.Entry("a", "0cc175b9c0f1b6a831c399e269772661"), r.Entry("b", "92eb5ffee6ae2fec3ad71c777531578f")]
+
+
+def test_aws_repo_can_retrieve_contents_by_path(aws_ab_repo):
+    contents = aws_ab_repo.contents('a')
+
+    assert contents.read() == b'a'
+    assert contents.content_id == "0cc175b9c0f1b6a831c399e269772661"
+
+
+def test_aws_repo_raises_key_error_if_file_doesnt_exist(aws_ab_repo):
+    with pytest.raises(KeyError):
+        aws_ab_repo.contents('c')
