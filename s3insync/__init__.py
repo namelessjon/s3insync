5c45e1f8a52221e5eecd0c43fb32676e775f7d82
Jon <jonathan.stott@gmail.com>
2021-01-23 15:52:35 +0000

    Version bump
    Also add in further descriptions

--- s3insync/__init__.py
+++ s3insync/__init__.py
@@ -1,6 +1,20 @@
 """
 Synchronise an S3 directory to the local filesystem, as a daemon
 
+Daemon to continuously and incrementally synchronize a directory from remote
+object store to a local directory.  Inspired by [objinsync](https://github.com/scribd/objinsync) but written in
+python and presently less fully featured.
+
+
+Usage
+-----
+
+```bash
+s3insync pull --exclude '*/__pycache__/*' s3://bucket/prefix ./localdir
+```
+
+When running in daemon mode, a prometheus metrics endpoint is served at
+`:8087/metrics`.
 """
 
-__version__ = '0.1.0'
+__version__ = '0.2.0'
